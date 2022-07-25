c5bce0014e4281d82e13dc1ab6331b5a08e8e64b
preet Mayani <pritmayani359@gmail.com>
2022-07-25 14:13:22 +0530

    Login in progress

--- lib/pages/main_login_signup_page.dart
+++ lib/pages/main_login_signup_page.dart
@@ -8,28 +8,78 @@ class LoginSignuppage extends StatelessWidget {
     return Material(
       child: Center(
         child: Container(
-          // decoration: BoxDecoration(
-          //   gradient: LinearGradient(
-          //     begin: Alignment.topRight,
-          //     end: Alignment.bottomLeft,
-          //     colors: [
-          //       Colors.red,
-          //       Colors.white
-          //     ]
-          //   )
-          // ),
           child: Column(
             children: [
               SizedBox(height: 70.0),
+              //Image
               Image.asset("assets/images/Welcome.png"),
+              //Text of welcome Start here
               Text(
-                "Welcome",
+                "Welcome Back👋",
                 style: TextStyle(
-                  //color: Colors.blue,
+                  color: Color.fromARGB(255, 80, 109, 255),
                   fontWeight: FontWeight.bold,
-                  fontSize: 30.0
+                  fontSize: 30.0,
                 ),
-                )
+                ),
+                Text(
+                "I am happy to see You can continue to \nlogin for manage your trades",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 78, 78, 78),
+                  //fontWeight: FontWeight.bold,
+                  fontSize: 13.0,
+                ),
+                ),
+
+
+              //Textfiled for the login page
+              SizedBox(height: 60.0),
+              ConstrainedBox(
+                constraints: const BoxConstraints.tightFor(width: 325),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon:Icon(Icons.person),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(width: 3,color: Colors.blue),
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(width: 3,color: Colors.blue),
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    labelText: "Username",
+                    hintText: "Enter UserName",
+                    hoverColor: Colors.blue
+                  ),
+                  
+                ),
+              ),
+              SizedBox(height: 30.0),
+              ConstrainedBox(
+                constraints: const BoxConstraints.tightFor(width: 325),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon:Icon(Icons.lock),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(width: 3,color: Colors.blue),
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(width: 3,color: Colors.blue),
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    labelText: "Password",
+                    hintText: "Please Enter Password",
+                    hoverColor: Colors.blue
+                  ),
+
+                ),
+              ),
+              //Adding Login Button Here
+
             ],
           ),
         ),
